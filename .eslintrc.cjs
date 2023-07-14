1c138bde8f46657ca044095d5250ce5ab00c8f20
Oleh Melnyk <oleh.melnyk@gmail.com>
2023-07-14 20:44:03 +0300

    chore: update npm packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,12 @@
 const config = {
   root: true,
   extends: ["@acme/eslint-config"], // uses the config in `packages/config/eslint`
+  rules: {
+    "@typescript-eslint/no-unsafe-assignment": "warn",
+    "@typescript-eslint/no-unsafe-member-access": "warn",
+    "@typescript-eslint/no-unsafe-call": "warn",
+    "@typescript-eslint/no-unsafe-return": "warn",
+  },
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
